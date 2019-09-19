0abbf5c4cb49564ba1928a9cabc8dc07f3df1a3f
NguyenMinhSang <nguyenminhsang5692@gmail.com>
2019-09-19 12:50:05 +0700

    Initial commit

---
+++ Linked-List/LinkedList.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_List
+{
+    class Node
+    {
+        public int data { get; set; }
+        public Node Next { get; set; }
+    }
+    class LinkedList
+    {
+        Node START;
+        
+        public void Insert(int item)
+        {
+            Node newNode = new Node();
+            newNode.data = item;
+            if (START == null)
+            {
+                START = newNode;
+            }
+            //else if (item < START.data)
+            //{
+            //    newNode.Next = START;
+            //    START = newNode;
+            //}
+            else
+            {
+                Node CurrentNode = START;
+                Node previous = null;
+                while (CurrentNode != null && CurrentNode.data <= item)
+                {
+                    previous = CurrentNode;
+                    CurrentNode = CurrentNode.Next;
+                }
+                if (previous.data != item)
+                {
+                    previous.Next = newNode;
+                    newNode.Next = CurrentNode;
+                }
+            }
+        }
+
+        public void Delete(int item)
+        {
+            Node DelNode = new Node();
+            DelNode.data = item;
+            if (START == null)
+            {
+                Console.WriteLine("Nothing");
+            }
+            //else if (START.data == item)
+            //{
+            //    START = START.Next;
+            //}
+            else
+            {
+                Node CurrentNode = START;
+                Node previous = null;
+                while (CurrentNode != null && CurrentNode.data != item)
+                {
+                    previous = CurrentNode;
+                    CurrentNode = CurrentNode.Next;
+                }
+                if (previous == null)
+                {
+                    START = START.Next;
+                }
+                else
+                {
+                    previous.Next = CurrentNode.Next;
+                }
+            }
+        }
+
+        public void Traverse()
+        {
+            Node CurrentNode = START;
+            while (CurrentNode != null)
+            {
+                Console.WriteLine(CurrentNode.data);
+                CurrentNode = CurrentNode.Next;
+            }
+        }
+    }
+}
---
+++ Linked-List/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList LList = new LinkedList();
+            LList.Insert(3);
+            LList.Insert(4);
+            LList.Insert(7);
+            LList.Insert(9);
+            LList.Insert(5);
+            LList.Insert(5);
+            LList.Insert(3);// loi neu them vao dau List
+
+            LList.Delete(3);
+            LList.Traverse();
+            Console.ReadLine();
+        }
+    }
+}
